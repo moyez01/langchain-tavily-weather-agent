fb93269054ea0594e48bb194e674ffebc396e265
Md. Moyez Uddin <43481673+moyez01@users.noreply.github.com>
2026-09-14 22:30:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# IMPORT
 from dotenv import load_dotenv
 import os
 from langchain_community.tools.tavily_search import TavilySearchResults
@@ -7,25 +8,25 @@ from langsmith import Client
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_classic.agents import create_react_agent, AgentExecutor
 
-
+# LOAD ENVIRONMENT
 load_dotenv()
 GEMENI_API_KEY = os.getenv('GEMENI_API_KEY')
 TAVILY_API_KEY = os.getenv('TAVILY_API_KEY')
 WEATHERSTACK_API_KEY = os.getenv("WEATHERSTACK_API_KEY")
 
-
+# LLM
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.6-flash",
     temperature=0,
     api_key=GEMENI_API_KEY
 )
 
-#Predefined TavilySearch Tools in Langchain
+# PREDEFINED TAVILY TOOL
 search_tool = TavilySearchResults(max_results=2)
 result = search_tool.invoke("Give me the Current weather at Dhaka")
 result
 
-#Custom Weather Data Tools
+# CUStOM WEATHER TOOL
 @tool
 def get_weather_data(city: str) -> str:
     """
@@ -46,29 +47,29 @@ def get_weather_data(city: str) -> str:
         f"Humidity: {data['current']['humidity']}%"
     )
 
-
+# TOOLS CALLING
 tools = [search_tool, get_weather_data]
 
-
+# ReAct PROMPT
 client = Client()
 # This modern client accepts the parameter and handles the safety barrier correctly
 prompt = client.pull_prompt("hwchase17/react", dangerously_pull_public_prompt=True)
 
-
+# CREATE AGENT
 agent = create_react_agent(
     llm= llm,
     tools=tools,
     prompt=prompt
 )
 
-
+# AGENT EXECUTOR
 agent_executor = AgentExecutor(
     agent=agent, 
     tools=tools,
     verbose=True
 )
 
-
+# INPUT
 response = agent_executor.invoke({
     "input": (
         "Find the capital of Bangladesh"
